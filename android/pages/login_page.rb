ffaab330a49e80204c27a166498d1484d26dd160
Renan Stofel <renanstofel10@gmail.com>
2019-04-09 18:32:44 -0300

    [add] tests android

---
+++ android/pages/login_page.rb
@@ -0,0 +1,67 @@
+require 'calabash-android/abase'
+
+# Class Login
+class loginpage < Calabash::ABase
+  def loginpage_view
+    query("* id:'login_form'")
+  end
+
+  def homepage_view
+    query("* id:'view_homepage'")
+  end
+
+  def usuario_text
+    query("* id:'user'")
+  end
+
+  def senha_text
+    query("* id:'password'")
+  end
+
+  def element_view(texto)
+    query("* text:'#{texto}'")
+  end
+
+  def wait_element(element)
+    wait_for do
+      element.count > 0
+    end
+  end
+
+  def load_loginpage(tempo)
+    wait_for do
+      loginpage_view.count > 0
+    end
+    sleep(tempo)
+  end
+
+  def load_homepage(tempo)
+    wait_for do
+      homepage_view.count > 0
+    end
+    sleep(tempo)
+  end
+
+  def load_message(mensagem)
+    fail "Erro: mensagem #{mensagem} não exibida!" unless element_view(mensagem).count > 0
+  end
+
+  def click_element(texto)
+    wait_element(element_view(texto))
+    touch(element_view(texto))
+  end
+
+  def preencher_login(usuario, senha)
+    for i in 0..1
+      if i.zero?
+        touch(usuario_text)
+        wait_for_keyboard
+        keyboard_enter_text(usuario)
+      else
+        touch(senha_text)
+        wait_for_keyboard
+        keyboard_enter_text(senha)
+      end
+    end
+  end
+end
--- tests/steps/calabash_steps.rb
+++ tests/steps/calabash_steps.rb
@@ -5,21 +5,23 @@ elsif ENV['PLATFORM'] == 'android'
 end
 
 Dado(/^que eu acesse a tela inicial$/) do
-	
+  @login = page(loginpage)
+  @login.load_loginpage(1)
 end
 
 Quando(/^digitar o usuário "([^\""]*)" e a senha "([^\""]*)"$/) do |usuario, senha|
-
+   @login.preencher_login(usuario, senha)
 end
 
-E(/^clicar no botão "([^\""]*)"$/) do |botao|
-
+E(/^clicar no botão "([^\""]*)"$/) do |texto|
+   @login.click_element(texto)
 end
 
 Então(/^a página principal é exibida$/) do
-
+  @home = page(homepage)
+  @home.load_homepage(1)
 end
 
 Então(/^a mensagem "([^\""]*)" é exibida$/) do |mensagem|
-
+  @login.load_message(mensagem)
 end
